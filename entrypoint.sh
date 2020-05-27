f3a803ca53afe72351020b8b8191ed68475bb2e6
BenEvenTsur <38428408+BenEvenTsur@users.noreply.github.com>
2020-05-27 14:01:59 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo ""
+echo "\033[1mNode version: $(node -v)\033[0m"
+echo "\033[1mNPM version: $(npm -v)\033[0m"
+echo "\033[1mReact version: 16.13.1\033[0m"
+echo "\033[1mTo start new porject run:\033[0m"
+echo "\033[1mcrao -n <app-name>\033[0m"
+echo ""
+exec /bin/bash
